0a77b4578172373cd289e86e0a11d7b69fa6db60
Logan Bellemare <ellabellemare@gmail.com>
2023-11-15 23:02:48 -0500

    finished lesson

--- src/palindromes/palindromes.js
+++ src/palindromes/palindromes.js
@@ -2,7 +2,19 @@ const Stack = require("../lib/stack");
 
 const isPalindrome = (sentence) => {
   sentence = sentence.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
-  // Your solution here
+  let middle = Math.floor(sentence.length / 2);
+  const compareStack = new Stack();
+  for (let char = 0; char < middle; char++) {
+    compareStack.push(sentence[char]);
+  }
+  middle += sentence.length % 2 === 0 ? 0 : 1;
+  for (let char = middle; char < sentence.length; char++) {
+    const popped = compareStack.pop();
+    if (sentence[char] !== popped) {
+      return false;
+    }
+  }
+  return true;
 };
 
 module.exports = isPalindrome;
--- src/parentheses/parentheses.js
+++ src/parentheses/parentheses.js
@@ -1,5 +1,21 @@
 const Stack = require("../lib/stack");
 
-const match = (expression) => {};
+const match = (expression) => {
+  const stack = new Stack();
+  for (let char = 0; char < expression.length; char++) {
+    if (expression[char] === "(") {
+      stack.push("(");
+    } else {
+      if (expression[char] === ")") {
+        if (stack.top) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+  return !stack.top;
+};
 
 module.exports = match;
